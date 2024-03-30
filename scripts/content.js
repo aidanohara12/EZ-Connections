3505c4f47d5556e443501fdd7ab78f60aaa6e969
Dylan Sparkenbaugh <djs231@pitt.edu>
2024-03-30 11:00:18 -0400

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -120,7 +120,7 @@ function highlightWord(categoryTitle){
     for(const tile of remainingWordsElements){
         console.log(tile.innerText);
         if(gameWords[solutionIndex].includes(tile.innerText)){
-            tile.style.backgroundColor = "rgb(219, 219, 210)";
+            tile.style.backgroundColor = "rgb(120, 120, 120)";
             break;
         }
     }
