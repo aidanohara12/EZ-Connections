dd4c4d37097d85ae42445b8ec6f1e2e9e1d849ea
Dylan22304 <114598396+Dylan22304@users.noreply.github.com>
2024-03-29 23:05:42 -0400

    Add the Hint Button to the Bottom Row

--- scripts/content.js
+++ scripts/content.js
@@ -13,14 +13,21 @@ let urlDate = yyyy + '-' + mm + '-' + dd;
 let url = "https://www.nytimes.com/svc/connections/v2/" + urlDate + ".json";
 
 //array to store the category titles
-let game_categories = [];
+let gameCategories = [];
 
 //import the json
 fetch(url).then((response) => response.json()).then((json) => {
     for(const category of json.categories){
-        game_categories.push(category.title);
+        gameCategories.push(category.title);
     }
 });
 
-const button = document.getElementById("portal-game-date");
-button.innerHTML = "YIPPEEE";
+//add the hint button to the webpage
+const buttonRow = document.getElementByClass("Board-module_boardActionGroup__mUDT8")
+const hintButton = document.createElement("button");
+hintButton.classList.add("ActionButton-module_button__IlhXt ");
+hintButton.style.background-color = "rgb(255, 255, 255)";
+hintButton.style.color = "rgb(127, 127, 127)";
+hintButton.style.border-color = "rgb(127, 127, 127)";
+hintButton.innerHTML = "Hint";
+rowButton.insertAdjacentElement("afterend", hintButton);
